46d0b451d3975355a1c52c2f2e726606a324a2d3
Dan Carmel <dan.carmel.mail@gmail.com>
2019-04-01 18:36:31 +0300

    commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,56 +1,77 @@
-import React, { Component } from 'react';
-import {getMovies} from "../services/fakeMovieService"
-
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 class Movies extends Component {
-    
-    state = {
-        movies: getMovies(),
-        inDataBase: true
-    
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(e => e._id !== movie._id);
+    this.setState({ movies });
+    console.log(movie);
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageLimit = () => {
+    const movies = [...this.state.movies];
+    // finish pagination excersize
+  };
+  render() {
+    if (this.state.movies.length !== 0) {
+      return (
+        <div>
+          <p>There are {this.state.movies.length} Movies in the Database</p>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination />
+        </div>
+      );
     }
-
-    handleDelete = (movie)=>{
-    const movies = this.state.movies.filter(e=>e._id !== movie._id)
-    this.setState({movies})
-    console.log(movie)
-    }
-
-    render() { 
-        
-      
-            if(this.state.movies.length !== 0){      
-        return ( 
-            <div>
-            <p>There are {this.state.movies.length} Movies in the Database</p>
-            {/* <h3 className="d-flex justify-content-center">{checkDB()}</h3> */}
-   
-            <table className="table">
-            <thead><tr>
-    <th>Name</th>
-    <th>Genre</th> 
-    <th>Stock</th>
-    <th>Rating</th>
-    </tr></thead><tbody>
-        
-            {
-    this.state.movies.map(movie => (
-            
-    <tr key={movie._id}>
-    <td>{movie.title}</td>
-    <td>{movie.genre.name}</td>
-    <td>{movie.numberInStock}</td>
-    <td>{movie.dailyRentalRate}</td>
-    <td><i className="fa fa-heart-o align-middle" aria-hidden="true"></i></td>
-    <td><button className="btn btn-danger" onClick={()=>this.handleDelete(movie)}>Delete</button></td>
-    </tr>
-
-    ))}
-            </tbody></table></div>
-         )
-    }return <p>There are no movies in the database.</p>
+    return <p>There are no movies in the database.</p>;
+  }
 }
-}  
- 
-export default Movies;
 
+export default Movies;
